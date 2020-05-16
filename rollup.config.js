78943b1d8144ac4797a40e056c28d495b69eb4e7
Mark Eklund <tab-sane@patnan.com>
2020-05-16 14:30:45 -0400

    Added css module support

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
+import postcss from 'rollup-plugin-postcss'
+
 
 import pkg from './package.json'
 
@@ -27,6 +29,10 @@ export default {
       exclude: 'node_modules/**'
     }),
     resolve(),
-    commonjs()
+    commonjs(),
+    postcss({
+      modules: true,
+      plugins: []
+    })
   ]
 }
--- src/HighlightWithinTextarea.js
+++ src/HighlightWithinTextarea.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles/styles.css';
 
 export const HighlightWithinTextarea = ({ height = '120px' , width = '260px' }) => (
-  <div>
+  <div className={styles.container}>
     <img
       style={{ height, width }}
       src='https://www.twilio.com/marketing/bundles/company/img/logos/red/twilio-logo-red.png'
