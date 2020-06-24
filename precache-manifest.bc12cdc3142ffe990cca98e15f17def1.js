810b850b952464590e6847bd0d665994b9e0fdf4
Mark Eklund <tab-sane@patnan.com>
2020-06-24 18:45:07 -0400

    Attempt to update docs

---
+++ precache-manifest.bc12cdc3142ffe990cca98e15f17def1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0320b5d53893cad69c7d675734dcdc4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1b2e74ac87dba865529",
+    "url": "./static/css/2.54f42587.chunk.css"
+  },
+  {
+    "revision": "34e69e618219e9769d07",
+    "url": "./static/css/main.28e3081b.chunk.css"
+  },
+  {
+    "revision": "b1b2e74ac87dba865529",
+    "url": "./static/js/2.008ef14b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.008ef14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e69e618219e9769d07",
+    "url": "./static/js/main.60ec3e3c.chunk.js"
+  },
+  {
+    "revision": "b3d5ebf8284a9aa43e40",
+    "url": "./static/js/runtime-main.5d49c890.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
