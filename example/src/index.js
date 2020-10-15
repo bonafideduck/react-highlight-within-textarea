fe5e784473dc8f4523c75082c9691fcdc3c97090
Mark Eklund <tab-sane@patnan.com>
2020-10-14 20:34:19 -0400

    updated packages

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
 
 import './index.css'
 import App from './App'
