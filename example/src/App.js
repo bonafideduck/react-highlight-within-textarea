fdc0597e773a24852d3de34e01c7876d7b2ffabe
Mark Eklund <tab-sane@patnan.com>
2021-03-03 08:58:56 -0500

    documentation enhancements

--- example/src/App.js
+++ example/src/App.js
@@ -95,7 +95,7 @@ let data = [[
     }
   }
 ], [
-  "Custom Object (with Class Name)",
+  "Custom Object with Class Name",
   <span>Any type mentioned here can be put in an object wrapper with <code>highlight</code> and <code>className</code> properties. This lets you set CSS classes in the highlight markup for custom styling, such as changing the highlight color.</span>,
   `Here's a blueberry. There's a strawberry. Surprise, it's a banananana!`,
   `[
@@ -128,7 +128,16 @@ let data = [[
   ],
 ], [
   "Custom Object with Enhancements",
-  <span>The div behind the highlighted span can be wrapped in an enhancement.</span>,
+  <span>
+  The div behind the highlighted span can be wrapped in an enhancement.
+  This React view receives properties, <code>data</code> which contains 
+  information about the particular span and <code>MarkView</code> which 
+  is the highlight that contains the text.  If there are
+  multiple enhancements, they will wrap around each other.  Also
+  note that a single highlight may be broken into two MarkViews if
+  it overlaps two highlights.  This effect can be seen below with
+  the two pairs of indexes.
+  </span>,
   `Here's a blueberry. There's a strawberry.  I'm a little blue because there is a highlight bug where a blueberry's highlight gets split over line breaks in Chrome incorrectly.  This makes me berry sad.
 But the blues go away after a newline is forced.`,
   `[
