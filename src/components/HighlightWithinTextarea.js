69f672bcc7dad63f9be1e69e229805fc318a5df6
Mark Eklund <tab-sane@patnan.com>
2021-05-22 14:25:31 -0400

    Added new draft-js based library

---
+++ src/components/HighlightWithinTextarea.js
@@ -0,0 +1,68 @@
+import { useState, forwardRef, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { Editor, EditorState, ContentState } from 'draft-js';
+import highlightToCompositeDecorator from './highlightToCompositeDecorator.js';
+
+const HighlightWithinTextarea = forwardRef((props, ref) => {
+  const { placeholder, highlight, onChange, value } = props;
+  let [onChangeState, setOnChangeState] = useState({
+    prev: { value: null, editorState: null },
+    next: { value: null, editorState: null },
+  });
+
+  let editorState;
+  if (value === onChangeState.next.value) {
+    editorState = onChangeState.next.editorState;
+  } else if (value === onChangeState.prev.value) {
+    editorState = onChangeState.prev.editorState;
+  } else {
+    const contentState = ContentState.createFromText(value);
+    editorState = EditorState.createWithContent(contentState);
+  }
+
+  const decorator = useMemo(
+    () => highlightToCompositeDecorator(highlight),
+    [highlight],
+  );
+  if (decorator !== editorState.decorator) {
+    editorState = EditorState.set(editorState, {
+      decorator: decorator,
+    });
+  }
+
+  const onDraftChange = (editorState) => {
+    const value = editorState.getCurrentContent().getPlainText();
+    setOnChangeState({
+      prev: onChangeState.next,
+      next: { value: value, editorState: editorState },
+    });
+    onChange(value);
+  };
+
+  return (
+    <Editor
+      editorState={editorState}
+      onChange={onDraftChange}
+      placeholder={placeholder}
+      ref={ref}
+    />
+  );
+});
+
+HighlightWithinTextarea.propTypes = {
+  onChange: PropTypes.func,
+  value: PropTypes.string.isRequired,
+  highlight: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array,
+    PropTypes.instanceOf(RegExp),
+  ]),
+  placeholder: PropTypes.string,
+};
+
+HighlightWithinTextarea.defaultProps = {
+  highlight: null,
+  placeholder: 'Enter some text...',
+};
+
+export default HighlightWithinTextarea;
---
+++ src/components/highlightToCompositeDecorator.js
@@ -0,0 +1,132 @@
+import { CompositeDecorator } from 'draft-js';
+import getType from './getType.js';
+
+const highlightToCompositeDecorator = (highlight) => {
+  let decorators = highlightToDecorator(
+    highlight,
+    undefined,
+    undefined,
+  );
+  const compositeDecorator = new CompositeDecorator(decorators);
+  return compositeDecorator;
+};
+
+const highlightToDecorator = (highlight, className, component) => {
+  const type = getType(highlight);
+
+  switch (type) {
+    case 'array':
+      return arrayToDecorator(highlight, className, component);
+    case 'function':
+      return functionToDecorator(highlight, className, component);
+    case 'regexp':
+      return regExpToDecorator(highlight, className, component);
+    case 'string':
+      return stringToDecorator(highlight, className, component);
+    case 'range':
+      return rangeToDecorator(highlight, className, component);
+    case 'custom':
+      return customToDecorator(highlight);
+    default:
+      if (!highlight) {
+        // do nothing for falsey values
+      } else {
+        console.error('unrecognized highlight type');
+      }
+      return [];
+  }
+};
+
+function arrayToDecorator(highlight, className, component) {
+  const decorators = highlight.map((h) =>
+    highlightToDecorator(h, className, component),
+  );
+  return Array.prototype.concat.apply([], decorators);
+}
+
+function functionToDecorator(highlight, className, component) {
+  return [
+    {
+      strategy: highlight,
+      component: hwtComponent(className, component),
+    },
+  ];
+}
+
+function regExpToDecorator(highlight, className, component) {
+  const regExpStrategy = (contentBlock, callback, contentState) => {
+    const text = contentBlock.getText();
+    let matchArr, start;
+    while ((matchArr = highlight.exec(text)) !== null) {
+      start = matchArr.index;
+      callback(start, start + matchArr[0].length);
+    }
+  };
+
+  return [
+    {
+      strategy: regExpStrategy,
+      component: hwtComponent(className, component),
+    },
+  ];
+}
+
+function stringToDecorator(highlight, className, component) {
+  const stringStrategy = (contentBlock, callback, callbackState) => {
+    const text = contentBlock.getText();
+
+    const textLower = text.toLowerCase();
+    const strLower = highlight.toLowerCase();
+    let index = 0;
+    while (
+      ((index = textLower.indexOf(strLower, index)), index !== -1)
+    ) {
+      callback(index, index + strLower.length);
+      index += strLower.length;
+    }
+  };
+
+  return [
+    {
+      strategy: stringStrategy,
+      component: hwtComponent(className, component),
+    },
+  ];
+}
+
+function rangeToDecorator(highlight, className, component) {
+  const rangeStrategy = (contentBlock, callback, callbackState) => {
+    const text = contentBlock.getText();
+    const low = highlight[0];
+    const high = highlight[1];
+    if (low < high && low >= 0 && high < text.length) {
+      callback(low, high);
+    }
+  };
+
+  return [
+    {
+      strategy: rangeStrategy,
+      component: hwtComponent(className, component),
+    },
+  ];
+}
+
+function customToDecorator(highlight) {
+  let className = highlight.class;
+  let component = highlight.component;
+  let hl = highlight.highlight;
+  return highlightToDecorator(hl, className, component);
+}
+
+const hwtComponent = (className, Component) => {
+  if (Component) {
+    return (props) => <Component className={className} {...props} />;
+  } else {
+    return (props) => (
+      <mark className={className}>{props.children}</mark>
+    );
+  }
+};
+
+export default highlightToCompositeDecorator;
