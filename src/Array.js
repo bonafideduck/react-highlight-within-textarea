89888538e49c3f11ba4f28f768835475ca1c1c54
Mark Eklund <tab-sane@patnan.com>
2021-05-31 12:14:52 -0400

    prettier and other stuff

--- src/Array.js
+++ src/Array.js
@@ -1,15 +1,19 @@
 import React from "react";
-import Example from "./Example.js"
-
+import Example from "./Example.js";
 
 const Text = () => {
-  return <Example
-    title="String"
-    text="Note that this is case-insensitive."
-    initialValue="Potato potato tomato potato."
-    highlightText={'"potato"'}
-    highlight="potato"
-  />
-}
+  return (
+    <>
+      <h2>String</h2>
+      <span>Note that this is case-insensitive.</span>
+
+      <Example
+        initialValue="Potato potato tomato potato."
+        highlightText={'"potato"'}
+        highlight="potato"
+      />
+    </>
+  );
+};
 
 export default Text;
--- src/Regexp.js
+++ src/Regexp.js
@@ -1,25 +1,19 @@
 import React from "react";
 import Example from "./Example.js";
 
-const Paragraph = () => {
-  return (
-    <span>
-      Don't forget the <code>g</code> (find all) and <code>i</code>{" "}
-      (case-insensitive) flags if you need them.
-    </span>
-  );
-};
-
 const Regexp = () => {
   return (
     <>
+      <h2>RegExp</h2>
+      <span>
+        Don't forget the <code>g</code> (find all) and <code>i</code>{" "}
+        (case-insensitive) flags if you need them.
+      </span>
       <Example
-        title="RegExp"
-        text={<Paragraph />}
         initialValue="Dog, cat, chicken, goose. Dogs, cats, chickens, geese."
         highlightText={`/dogs?|cats?|g(oo|ee)se/gi`}
         highlight={/dogs?|cats?|g(oo|ee)se/gi}
-      />
+      ></Example>
     </>
   );
 };
