eb5c45df7cdc5ed71904332180a5dd973f003ed3
Mark Eklund <tab-sane@patnan.com>
2021-06-03 07:51:45 -0400

    checkpoint commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.querySelector('#demo')
+  document.querySelector("#demo")
 );
 
 // If you want to start measuring performance in your app, pass a function
